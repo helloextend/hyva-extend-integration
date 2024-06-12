f96a95cf29e2f00e55c6fdcbcd044028d2a9cf0b
Jean-Michel Batty <jean-michel.batty@extend.com>
2024-06-12 16:52:38 -0500

    simplification and optimizations
    Got rid of alpinejs logic firing too late while hyva loads faster
    
    ** note that you need at minimum helloextend/integration:v3.10.0

--- src/view/frontend/web/js/utils/cart-utils.js
+++ src/view/frontend/web/js/utils/cart-utils.js
@@ -6,32 +6,34 @@
 
     'use strict';
 
-    function cart_utils(customerData) {
-        return {
-            getCartItems: function () {
-                return customerData.cart.items ?? []
-            },
-            getCartData: function () {
-                 return customerData.cart;
-            },
-            refreshMiniCart: function () {
-                hyva.setCookie('mage-cache-sessid', '', -1, true); // remove the cookie
-                window.dispatchEvent(new CustomEvent("reload-customer-section-data")); // reload the data
-            },
-            mapToExtendCartItem: function (magentoCartItem) {
-                return {
-                    name: magentoCartItem.product_name,
-                    sku: magentoCartItem.product_sku,
-                    qty: magentoCartItem.qty,
-                    price: magentoCartItem.product_price_value * 100,
-                    item_id: magentoCartItem.product_id,
-                    options: [],
-                }
-            },
-        }
-    }
+window.cart_utils = (customerData) => {
+    return {
+        getCartItems: function () {
 
-    //window.addEventListener('private-content-loaded', initCartUtils);
-    export {cart_utils as default};
+            if (customerData.cart && customerData.cart.items){
+                return customerData.cart.items ;
+            }else{
+                return [];
+            }
+        },
+        getCartData: function () {
+            return customerData.cart;
+        },
+        refreshMiniCart: function () {
+            hyva.setCookie('mage-cache-sessid', '', -1, true); // remove the cookie
+            window.dispatchEvent(new CustomEvent("reload-customer-section-data")); // reload the data
+        },
+        mapToExtendCartItem: function (magentoCartItem) {
+            return {
+                name: magentoCartItem.product_name,
+                sku: magentoCartItem.product_sku,
+                qty: magentoCartItem.qty,
+                price: magentoCartItem.product_price_value * 100,
+                item_id: magentoCartItem.product_id,
+                options: [],
+            }
+        },
+    }
+}
 
 
